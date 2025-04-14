c97fea8f33ffb5aca3dac825d0cc94b9d903b3e8
Justice <ezejustice112@gmail.com>
2025-04-14 18:28:29 +0100

    recent commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/SwiftCare-App/",
+  base:"/SwiftCare-WebApp/",
   plugins: [react()],
  
 })
