474dc21d6ba702c4843a787fa22950436d3b6e42
Justice <ezejustice112@gmail.com>
2025-05-02 10:42:06 +0100

    updated chnages

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import SwiftCare from "./SwiftCare";
 import ConsultPage from "./SubHeader/Consult";
 import HealthSupport from "./SubHeader/Health";
-import Login from "./LoginSection/Login";
 import "./index.css";
-import Register from "./LoginSection/register";
 import MentalService from "./SubHeader/Mental";
 
 const App = () => {
@@ -15,8 +13,6 @@ const App = () => {
       <Route path="/consult" element={<ConsultPage />} />
       <Route path="/health" element={<HealthSupport />} />
       <Route path="/mental" element={<MentalService/>} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
